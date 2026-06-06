65a878a06a548eaa7dd6c7f4b2af5ba6ed1dac0a
shoxijahon004 <shoxijahon004@gmail.com>
2026-06-07 02:38:06 +0500

    Add emotion detection function to analyze text

---
+++ emotion_detection.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+        url = 'https://skills.network'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    
+    response = requests.post(url, json=myobj, headers=headers)
+    
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None

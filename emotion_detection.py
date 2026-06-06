fbcf30861cd62a085acef0fd11fe276e443973c8
shoxijahon004 <shoxijahon004@gmail.com>
2026-06-07 02:41:18 +0500

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,12 +2,19 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-        url = 'https://skills.network'
+    # Watson NLP API manzili (To'liq ko'rinishi)
+    url = 'https://skills.network'
+    
+    # API uchun sarlavha
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
+    # Yuboriladigan matn ma'lumoti
     myobj = { "raw_document": { "text": text_to_analyze } }
     
+    # API'ga so'rov yuborish
     response = requests.post(url, json=myobj, headers=headers)
     
+    # Agar so'rov muvaffaqiyatli bo'lsa, natijani qaytarish
     if response.status_code == 200:
         return response.text
     else:
